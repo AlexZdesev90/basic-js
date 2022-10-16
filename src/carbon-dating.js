5fd0a392a7020b68847b39ffe74342325f2750fd
AlexZdesev90 <zdesev-aleksandr@mail.ru>
2022-10-16 18:53:12 +0300

    feat:  fix carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,16 +18,9 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  if(typeof sampleActivity !== "string" ) return false;
+  if(typeof sampleActivity !== "string" || sampleActivity < 0 || sampleActivity > MODERN_ACTIVITY) return false;
     str = +sampleActivity;
-    if(str <= 0) return false;
-
-     //tests
-     // dateSample('9000'), false);
-    // dateSample('15.1'), false);
-    // dateSample('0'), false);
-    //dateSample('-5'), false);
-    //dateSample('-55.8'), false);
+    if(str <= 0 ) return false;
     
     const k =0.693/HALF_LIFE_PERIOD;
     
